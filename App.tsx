021de49ac9da92854fe6793b192a36eec7b0cb4d
ThiagoCosta <thiagohrcosta>
2022-09-22 22:47:50 -0300

    Adding global styles

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Dashboard } from './src/screens/Dashboard';
 
+import { ThemeProvider } from 'styled-components';
+import theme from './src/global/styles/theme';
+
 export default function App() {
   return (
-    <Dashboard />
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
   );
 }
