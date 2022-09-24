cdb04d7a2d7f5b9d374125384c6d9c8a19e0afd4
ThiagoCosta <thiagohrcosta>
2022-09-23 22:15:31 -0300

    Adding header component

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Text } from 'react-native';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  UserWrapper
+ } from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{uri: 'https://avatars.githubusercontent.com/u/28869405?v=4'}}/>
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Thiago</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
