496e714feb0eb809a69ea066e54d6099551ece92
ThiagoCosta <thiagohrcosta>
2022-09-24 13:51:12 -0300

    Adding styles to card content

--- src/components/TransactionCard/styled.ts
+++ src/components/TransactionCard/styled.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
 import { Feather } from '@expo/vector-icons'
-import { RFValue } from '@react-native-responsive-fontsize'
+import { RFValue } from 'react-native-responsive-fontsize'
 
 export const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.shape};
@@ -9,31 +9,42 @@ export const Container = styled.View`
 `;
 
 export const Title = styled.Text`
-  font-size
-
+  font-size: ${RFValue(14)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
 `;
 
 export const Amount = styled.Text`
-
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(20)}px;
+  margin-top: 2px;
 `;
 
 export const Footer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 
+  margin-top: 19px;
 `;
 
 export const Category = styled.View`
-
+  flex-direction: row;
+  align-items: center;
 `;
 
 export const CategoryName = styled.Text`
-
+  font-size: ${RFValue(14)}px;
+  color: ${({ theme }) => theme.colors.text};
+  margin-left: 17px;
 `;
 
 export const Icon = styled(Feather)`
-
+  font-size: ${RFValue(20)}px;
+  color: ${({ theme }) => theme.colors.text};
 `;
 
 export const Date = styled.Text`
-
+  font-size: ${RFValue(14)}px;
+  color: ${({ theme }) => theme.colors.text};
 `;
 
