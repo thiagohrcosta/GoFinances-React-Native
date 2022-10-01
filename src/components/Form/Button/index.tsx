b838b26e85133278b100fa35c6ac89f823e51591
ThiagoCosta <thiagohrcosta>
2022-09-30 22:11:27 -0300

    Adding modal

--- src/components/Form/Button/index.tsx
+++ src/components/Form/Button/index.tsx
@@ -8,7 +8,7 @@ interface Props extends TouchableOpacityProps {
 
 export function Button({ title, ...rest }: Props) {
   return (
-    <Container>
+    <Container {...rest}>
       <Title>{title}</Title>
     </Container>
   );
--- src/screens/CategorySelect/index.tsx
+++ src/screens/CategorySelect/index.tsx
@@ -21,7 +21,7 @@ interface Category {
 }
 
 interface Props {
-  category: string;
+  category: Category;
   setCategory: (category: Category) => void;
   closeSelectCategory: () => void;
 }
@@ -45,7 +45,10 @@ export function CategorySelect({category, setCategory, closeSelectCategory}: Pro
         ItemSeparatorComponent={() => <Separator />}
       />
       <Footer>
-        <Button title="Selecionar" />
+        <Button
+          title="Selecionar"
+          onPress={closeSelectCategory}
+        />
       </Footer>
     </Container>
   );
