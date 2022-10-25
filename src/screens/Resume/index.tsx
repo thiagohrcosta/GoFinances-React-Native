f2e5b377ad86b97c17b4913593ce60b417b54c72
ThiagoCosta <thiagohrcosta>
2022-10-25 19:46:20 -0300

    Adding filter by month

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -7,7 +7,11 @@ import {
   Header,
   Title,
   Content,
-  ChartContainer
+  ChartContainer,
+  MonthSelect,
+  MonthSelectButton,
+  MonthSelectIcon,
+  Month,
 } from "./styles";
 import { categories } from "../../utils/categories";
 
@@ -15,6 +19,7 @@ import { VictoryPie } from "victory-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
 import { useTheme } from "styled-components";
+import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 
 interface TransactionData {
   type: "positive" | "negative";
@@ -92,7 +97,24 @@ export function Resume() {
       <Header>
         <Title>Resumo por categoria</Title>
       </Header>
-      <Content>
+      <Content
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingHorizontal: 24,
+          paddingBottom: useBottomTabBarHeight(),
+        }}
+      >
+        <MonthSelect>
+          <MonthSelectButton>
+            <MonthSelectIcon name="chevron-left" />
+          </MonthSelectButton>
+
+          <Month>Maio</Month>
+
+          <MonthSelectButton>
+            <MonthSelectIcon name="chevron-right" />
+          </MonthSelectButton>
+        </MonthSelect>
         <ChartContainer>
           <VictoryPie
             data={totalByCategories}
--- src/screens/Resume/styles.ts
+++ src/screens/Resume/styles.ts
@@ -1,5 +1,6 @@
 import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
+import { Feather } from "@expo/vector-icons";
 
 export const Container = styled.View`
     flex: 1;
@@ -15,10 +16,7 @@ export const Header = styled.View`
     padding-bottom: 19px;
 `;
 
-export const Content = styled.ScrollView.attrs({
-  contentContainerStyle: { padding: 24, flex: 1 }
-})`
-`;
+export const Content = styled.ScrollView``;
 
 export const Title = styled.Text`
   color: ${({ theme }) => theme.colors.shape};
@@ -29,4 +27,26 @@ export const Title = styled.Text`
 export const ChartContainer = styled.View`
   width: 100%;
   align-items: center;
+`;
+
+export const MonthSelect = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  margin-top: 24px;
+`;
+
+export const MonthSelectButton = styled.Text`
+
+`;
+
+export const MonthSelectIcon = styled(Feather)`
+  font-size: ${RFValue(24)}px;
+`;
+
+export const Month = styled.Text`
+  font-size: ${RFValue(20)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
 `;
