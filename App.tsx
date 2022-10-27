de754a8b172adf9448a9d3615307c25c35c9a06f
ThiagoCosta <thiagohrcosta>
2022-10-27 19:15:59 -0300

    Adding login screen style

--- App.tsx
+++ App.tsx
@@ -16,6 +16,8 @@ import {
   Poppins_700Bold
 } from '@expo-google-fonts/poppins';
 import { AppRoutes } from './src/routes/app.routes';
+import { SignIn } from './src/screens/SignIn';
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -31,7 +33,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle="light-content"/>
-        <AppRoutes />
+        <SignIn />
       </NavigationContainer>
     </ThemeProvider>
   );
